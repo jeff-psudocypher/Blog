0fc0d356dd239e448a817af83c89b4fd19527369
jeff-psudocypher <ajgj2018@gmail.com>
2024-10-22 10:03:36 +0530

    postscrud

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from blogs.models import Category,Blog
 from django.contrib.auth.decorators import login_required
-from .forms import CategoryForm
+from .forms import CategoryForm,BlogForm
+from django.template.defaultfilters import slugify
 # Create your views here.
 
 @login_required(login_url='login')
@@ -39,3 +40,39 @@ def delete_category(request,pk):
     category = get_object_or_404(Category,pk=pk)
     category.delete()
     return redirect('categories')
+
+def posts(request):
+    posts=Blog.objects.all()
+    return render(request,'dashboard/posts.html',{'posts':posts})
+
+def add_posts(request):
+    if(request.method == 'POST'):
+        form = BlogForm(request.POST,request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False) # temporarly saving the form
+            post.blog_author = request.user
+            post.save()
+            title = form.cleaned_data['blog_title']
+            post.slug = slugify(title) + '-' + str(post.id) #this-is-a-new-post
+            post.save()
+            return redirect('posts')
+    form = BlogForm()
+    return render(request,'dashboard/add_post.html',{'form':form})
+
+def edit_posts(request,pk):
+    post=get_object_or_404(Blog,pk=pk)
+    if (request.method == 'POST'):
+        form = BlogForm(request.POST,request.FILES,instance=post)
+        if form.is_valid():
+            post=form.save()
+            title=form.cleaned_data['blog_title']
+            post.slug = slugify(title) + '-' + str(post.id) #this-is-a-new-post
+            post.save()
+            return redirect('posts')
+    form = BlogForm(instance=post)
+    return render(request,'dashboard/edit_post.html',{'form':form,'post':post})
+
+def delete_posts(request,pk):
+    post=get_object_or_404(Blog,pk=pk)
+    post.delete()
+    return redirect('posts')
